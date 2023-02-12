84cff165d14e69ffd6c7b8bab1089fc438969f9e
Roman Levin <romanvalevin@gmail.com>
2023-02-12 16:09:57 +0300

    added few funcs for editing graph (deleting edges and nodes and editing weigth and nodes)

--- Graph.h
+++ Graph.h
@@ -12,48 +12,123 @@ using namespace std;
 template <typename T>
 class Graph{
 private:
-    int _numberOfNodes;
     vector<T> _nodes;
     vector<vector<pair<int, int>>> _adjList; // adjacency list of pairs like (nodeIndex, weight)
+
+    //=====================HIDDEN METHODS==========================//
+    bool HasCycleRe(bool visited[], const int& nodeIndex, const int& parent = -1) const{
+        visited[nodeIndex] = true;
+        cout << nodeIndex << " nodeIndex " << parent << " parent\n";
+        for (int i = 0; i < _adjList[nodeIndex].size(); i++){
+            int nodeIndex2 = (_adjList[nodeIndex])[i].first;
+            //cout << "iter " << i << endl;
+            if (!visited[nodeIndex2])
+                HasCycleRe(visited, nodeIndex2, nodeIndex);
+            else if (nodeIndex2 != parent) {
+                //cout << nodeIndex2 << " " << parent << endl;
+                return true;
+            }
+        }
+        return false;
+    }
+
 public:
     //=======================CONSTRUCTORS==========================//
 
-    Graph() {_numberOfNodes = 0;}
-    Graph(int numberOfNodes) : _numberOfNodes(numberOfNodes), _adjList(numberOfNodes), _nodes(numberOfNodes){}
+    Graph() {}
+    Graph(int numberOfNodes) : _adjList(numberOfNodes), _nodes(numberOfNodes){}
     ~Graph() = default;
 
-    //======================GRAPH METHODS===========================//
+    //=========================METHODS===========================//
 
-    void addEdge(const int& firstNodeIndex, const int& secondNodeIndex, const int& weight){
+    void AddEdge(const int& firstNodeIndex, const int& secondNodeIndex, const int& weight){
+        if (firstNodeIndex < 0 || firstNodeIndex >= _nodes.size() ||
+            secondNodeIndex < 0 || secondNodeIndex >= _nodes.size())
+            throw "Incorrect Index";
         _adjList[firstNodeIndex].push_back(make_pair(secondNodeIndex, weight));
     }
 
-    void addNode(const T& newNode){
-        _nodes.push_back(newNode);
-        _numberOfNodes++;
-        _adjList.resize(_adjList.size()+1);
+    void ChangeEdgeWeight(const int& firstNodeIndex, const int& secondNodeIndex, const int& weight){
+        for (auto it = _adjList[firstNodeIndex].begin(); it != _adjList[firstNodeIndex].end(); it++){
+            if ((*it).first == secondNodeIndex) {
+                (*it).second = weight;
+                return;
+            }
+        }
+        throw "No Edge";
+    };
+
+    void DeleteEdge(const int& firstNodeIndex, const int& secondNodeIndex){
+        if (firstNodeIndex < 0 || firstNodeIndex >= _nodes.size())
+            return;
+        auto it = _adjList[firstNodeIndex].begin();
+        while(it != _adjList[firstNodeIndex].end()){
+            if ((*it).first == secondNodeIndex){
+                _adjList[firstNodeIndex].erase(it);
+                return;
+            }
+            it++;
+        }
     }
 
-    int getEdgeWeight(const int& firstNodeIndex, const int& secondNodeIndex) const{
+    int GetEdgeWeight(const int& firstNodeIndex, const int& secondNodeIndex) const{
         for (auto it = _adjList[firstNodeIndex].begin(); it < _adjList[firstNodeIndex].end(); it++)
             if ((*it).first == secondNodeIndex)
                 return (*it).second;
         return INF;
     }
 
-    T getNode(int nodeIndex) const{
-        if (nodeIndex < 0 || nodeIndex >= _nodes.size()) throw "incorrect index";
+    void AddNode(const T& newNode){
+        _nodes.push_back(newNode);
+        _adjList.resize(_adjList.size()+1);
+    }
+
+    void ChangeNode(const int& nodeIndex, const T& newNode){
+        if (nodeIndex < 0 || nodeIndex >= _nodes.size())
+            throw "Invalid Index";
+        _nodes[nodeIndex] = newNode;
+    }
+
+    void DeleteNode(const int& nodeIndex){
+        if (nodeIndex < 0 || nodeIndex >= _nodes.size())
+            throw "Invalid Index";
+        auto it1 = _nodes.begin();
+        auto it2 = _adjList.begin();
+        int counter = 0;
+        while (it1 != _nodes.end()){
+            if (counter == nodeIndex) {
+                _nodes.erase(it1);
+                _adjList.erase(it2);
+                return;
+            }
+            counter++;
+            it1++;
+            it2++;
+        }
+    }
+
+    T GetNode(int nodeIndex) const{
+        if (nodeIndex < 0 || nodeIndex >= _nodes.size()) throw "Incorrect Index";
         return _nodes[nodeIndex];
     }
 
     int GetSize() const{
-        return _numberOfNodes;
+        return _nodes.size();
+    }
+
+    bool HasCycle(){
+        bool visited[_nodes.size()];
+        for (int i = 0; i < _nodes.size(); i++)
+            if (!visited[i])
+                if (HasCycleRe(visited, i))
+                    return true;
+        return false;
     }
 
     //========================OPERATORS=========================//
 
     T& operator[] (int nodeIndex){
-        if (nodeIndex < 0 || nodeIndex >= _nodes.size()) throw "incorrect index";
+        if (nodeIndex < 0 || nodeIndex >= _nodes.size()) throw "Incorrect Index";
         return _nodes[nodeIndex];
     }
 
@@ -62,8 +137,8 @@ public:
         for (int i = 0; i < size; i++) {
             os << i << " : { ";
             for (int j = 0; j < size; j++) {
-                if (graph.getEdgeWeight(i, j) != INF)
-                    os << "(" << j << "," << graph.getEdgeWeight(i, j) << ") ";
+                if (graph.GetEdgeWeight(i, j) != INF)
+                    os << "(" << j << "," << graph.GetEdgeWeight(i, j) << ") ";
             }
             os << "};\n";
         }
--- main.cpp
+++ main.cpp
@@ -2,17 +2,40 @@
 #include "Graph.h"
 using namespace std;
 int main() {
-    Graph<char> graph = Graph<char>(4);
-    graph[0] = 'a';
-    graph[1] = 'b';
-    graph[2] = 'c';
-    graph[3] = 'd';
-    graph.addEdge(0, 1, 13);
-    graph.addEdge(0, 2, 4);
-    graph.addEdge(1, 3, 10);
-    graph.addEdge(2, 1, 9);
+    Graph<char> graph = Graph<char>();
+    //graph[0] = 'a';
+    //graph[1] = 'b';
+    //graph[2] = 'c';
+    //graph[3] = 'd';
+    graph.AddNode('a');
+    graph.AddNode('b');
+    graph.AddNode('c');
+    graph.AddNode('d');
+    graph.AddEdge(0, 1, 13);
+    graph.AddEdge(0, 2, 4);
+    graph.AddEdge(1, 3, 10);
+    graph.AddEdge(2, 1, 9);
+    graph.AddEdge(3, 0, 2);
     //cout << graph.getNode(0);
     cout << graph << endl;
+
+
+    //graph.DeleteEdge(3, 0);
+    //graph.ChangeNode(3, 'l');
+
+    //cout << graph << endl;
+
+
+    graph.DeleteNode(3);
+
+    cout << graph << endl;
+/*
+    graph.ChangeEdgeWeight(0, 2, 5);
+    graph.AddNode('n');*/
+
+    //cout << graph << endl;
+
+    //cout << graph.HasCycle() << endl;
 
     return 0;
 }
